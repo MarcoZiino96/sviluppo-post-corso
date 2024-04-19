8c7ecfe3aaffcace817db94ab1975c6aff291aa9
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-04-19 17:05:44 +0200

    esercitazione sul rendering condizionale react e funzionalità base

--- corso-react-Midali/src/App.tsx
+++ corso-react-Midali/src/App.tsx
@@ -9,16 +9,19 @@ function App() {
           <Card
             title="Tokyo"
             img="https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            isVisited ={false}
           ></Card>
 
           <Card
             title="Rome"
             img="https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1396&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            isVisited ={false}
           ></Card>
 
           <Card
             title="Berlino"
             img="https://images.unsplash.com/photo-1599946347371-68eb71b16afc?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            isVisited ={false}
           ></Card>
         </div>
       </div>
--- corso-react-Midali/src/components/Card.tsx
+++ corso-react-Midali/src/components/Card.tsx
@@ -1,18 +1,21 @@
 type CardProps = {
   title: string;
   img: string;
+  isVisited: boolean;
 };
 
-function Card({ title, img }: CardProps) {
+function Card({ title, img, isVisited }: CardProps) {
   const description: string =
     "lorem ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit";
   return (
     <div className=" text-center my-5">
-      <img className="h-32 w-32 m-auto" src={img} alt="" />
+      <img className="w-auto h-auto m-auto" src={img} alt="" />
       <div className="w-auto">
         <div className="bg-blue-50">
           <h2 className="text-2xl text-black font-bold">{title}</h2>
           <p className="text-gray-500">{description}</p>
+          {isVisited &&  <span> Visitata</span>}
+          {!isVisited &&  <span> Non Visitata</span>}
         </div>
       </div>
     </div>
