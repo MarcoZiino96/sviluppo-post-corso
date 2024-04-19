39aba69acf570fd52ea61a9133325b5a9434d070
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-04-19 18:21:18 +0200

    rendering di liste con react esercitazione

--- corso-react-Midali/src/App.tsx
+++ corso-react-Midali/src/App.tsx
@@ -2,15 +2,50 @@ import "./App.scss";
 import Card from "./components/Card";
 
 function App() {
+
+  const cities = [
+    {
+      id:0,
+      title:"Tokyo",
+      img:"https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited : false,
+      description : "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet"
+    },
+    {
+      id:1,
+      title:"Roma",
+      img:"https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1396&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited : true,
+      description : "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet"
+
+    },
+    {
+      id:2,
+      title:"Berlino",
+      img:"https://images.unsplash.com/photo-1599946347371-68eb71b16afc?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited : false,
+      description : "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet"
+
+    },
+    {
+      id:3,
+      title:"Londra",
+      img:"https://images.unsplash.com/photo-1533929736458-ca588d08c8be?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited : true,
+      description : "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet"
+
+    },
+  ]
   return (
     <>
       <div className=" container m-auto">
         <div className="flex gap-5">
-          <Card
+          {/* <Card
             title="Tokyo"
             img="https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             isVisited ={false}
-          ></Card>
+            description="lorem ipsum dolor sit amet dolor sit amet dolor sit"
+          ></Card> 
 
           <Card
             title="Rome"
@@ -22,7 +57,27 @@ function App() {
             title="Berlino"
             img="https://images.unsplash.com/photo-1599946347371-68eb71b16afc?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             isVisited ={false}
-          ></Card>
+          ></Card> */}
+          {/* {cities.map((city)=>(
+            <Card
+            id={city.id}
+            title={city.title}
+            img={city.img}
+            isVisited={city.isVisited}>
+            {city.description}  
+            </Card>
+          ))} */}
+          {cities.filter((city)=> !city.isVisited)
+              .map((city)=>(
+                <Card
+                id={city.id}
+                title={city.title}
+                img={city.img}
+                isVisited={city.isVisited}>
+                  {city.description} 
+                </Card>
+              ))
+            }
         </div>
       </div>
     </>
--- corso-react-Midali/src/components/Card.tsx
+++ corso-react-Midali/src/components/Card.tsx
@@ -1,19 +1,22 @@
-type CardProps = {
+import { ReactNode } from "react";
+
+interface CardProps{
+  id:number;
   title: string;
   img: string;
   isVisited: boolean;
-};
+  children?:ReactNode
+}
 
-function Card({ title, img, isVisited }: CardProps) {
-  const description: string =
-    "lorem ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit amet ipsum dolor sit";
+function Card({ title, img, isVisited, children}: CardProps) {
+
   return (
     <div className=" text-center my-5">
-      <img className="w-auto h-auto m-auto" src={img} alt="" />
+      <img className=" w-auto m-auto" src={img} alt="" />
       <div className="w-auto">
-        <div className="bg-blue-50">
+        <div className="bg-blue-50 p-5">
           <h2 className="text-2xl text-black font-bold">{title}</h2>
-          <p className="text-gray-500">{description}</p>
+          <p className="text-gray-500">{children}</p>
           {isVisited &&  <span> Visitata</span>}
           {!isVisited &&  <span> Non Visitata</span>}
         </div>
