2100cb248ef679f6c9d932efb7aa7d784353e6e6
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-04-20 17:12:25 +0200

    gestione dello stato e gestione degli eventi in app react

--- corso-react-Midali/src/App.tsx
+++ corso-react-Midali/src/App.tsx
@@ -1,83 +1,102 @@
+import { useState } from "react";
 import "./App.scss";
 import Card from "./components/Card";
 
 function App() {
-
+  
+  const [showVisited, setShowVisite] = useState(false);
+  const [showNotVisited, setShowNotVisite] = useState(false);
   const cities = [
     {
-      id:0,
-      title:"Tokyo",
-      img:"https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      isVisited : false,
-      description : "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet"
+      id: 0,
+      title: "Tokyo",
+      img: "https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: false,
+      description:
+        "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet",
     },
     {
-      id:1,
-      title:"Roma",
-      img:"https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1396&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      isVisited : true,
-      description : "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet"
-
+      id: 1,
+      title: "Roma",
+      img: "https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1396&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: true,
+      description:
+        "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet",
     },
     {
-      id:2,
-      title:"Berlino",
-      img:"https://images.unsplash.com/photo-1599946347371-68eb71b16afc?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      isVisited : false,
-      description : "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet"
-
+      id: 2,
+      title: "Berlino",
+      img: "https://images.unsplash.com/photo-1599946347371-68eb71b16afc?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: false,
+      description:
+        "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet",
     },
     {
-      id:3,
-      title:"Londra",
-      img:"https://images.unsplash.com/photo-1533929736458-ca588d08c8be?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      isVisited : true,
-      description : "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet"
-
+      id: 3,
+      title: "Londra",
+      img: "https://images.unsplash.com/photo-1533929736458-ca588d08c8be?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: true,
+      description:
+        "lorem ipsum dolor sit amet, consect dolor sit amet, sed diam dolor sit amet",
     },
-  ]
+  ];
+
+  function NotVisited(){
+
+    return cities.filter((city) => !city.isVisited).map((city) => (
+      <Card
+        key={city.id}
+        title={city.title}
+        img={city.img}
+        isVisited={city.isVisited}
+      >
+        {city.description}
+      </Card>
+    ))
+   
+  }
+
+  function visited(){
+
+    return cities.filter((city) => city.isVisited).map((city) => (
+      <Card
+        key={city.id}
+        title={city.title}
+        img={city.img}
+        isVisited={city.isVisited}
+      >
+        {city.description}
+      </Card>
+    ))
+   
+  }
   return (
     <>
       <div className=" container m-auto">
-        <div className="flex gap-5">
-          {/* <Card
-            title="Tokyo"
-            img="https://images.unsplash.com/photo-1503899036084-c55cdd92da26?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            isVisited ={false}
-            description="lorem ipsum dolor sit amet dolor sit amet dolor sit"
-          ></Card> 
-
-          <Card
-            title="Rome"
-            img="https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1396&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            isVisited ={false}
-          ></Card>
-
-          <Card
-            title="Berlino"
-            img="https://images.unsplash.com/photo-1599946347371-68eb71b16afc?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            isVisited ={false}
-          ></Card> */}
-          {/* {cities.map((city)=>(
+        <div className="grid grid-cols-2 justify-items-center">
+          
+          {cities.map((city) => (
             <Card
-            id={city.id}
-            title={city.title}
-            img={city.img}
-            isVisited={city.isVisited}>
-            {city.description}  
+              key={city.id}
+              title={city.title}
+              img={city.img}
+              isVisited={city.isVisited}
+            >
+              {city.description}
             </Card>
-          ))} */}
-          {cities.filter((city)=> !city.isVisited)
-              .map((city)=>(
-                <Card
-                id={city.id}
-                title={city.title}
-                img={city.img}
-                isVisited={city.isVisited}>
-                  {city.description} 
-                </Card>
-              ))
-            }
+          ))}
+
+          <button className="bg-blue-500 p-4" onClick={()=> setShowVisite(!showVisited)}>
+          {showVisited ? 'Nascondi Città visitate' : 'Mostra Città visitate'}
+          </button>
+          <button className="bg-blue-500 p-4" onClick={()=> setShowNotVisite(!showNotVisited)}>
+          {showNotVisited ? 'Nascondi Città visitate' : 'Mostra Città visitate'}
+          </button>
+        </div>
+
+        <div className="grid grid-cols-2 justify-items-center">
+          {showVisited && visited()}
+          {showNotVisited && NotVisited()}
         </div>
       </div>
     </>
--- corso-react-Midali/src/components/Card.tsx
+++ corso-react-Midali/src/components/Card.tsx
@@ -1,27 +1,26 @@
 import { ReactNode } from "react";
 
 interface CardProps{
-  id:number;
+  key:number;
   title: string;
   img: string;
   isVisited: boolean;
-  children?:ReactNode
+  children:ReactNode
 }
 
 function Card({ title, img, isVisited, children}: CardProps) {
 
   return (
-    <div className=" text-center my-5">
-      <img className=" w-auto m-auto" src={img} alt="" />
-      <div className="w-auto">
-        <div className="bg-blue-50 p-5">
-          <h2 className="text-2xl text-black font-bold">{title}</h2>
-          <p className="text-gray-500">{children}</p>
-          {isVisited &&  <span> Visitata</span>}
-          {!isVisited &&  <span> Non Visitata</span>}
+    <div className="boxCard bg-current text-center my-5">
+      <img className=" imgCard  m-auto" src={img} alt="" />
+        <div className="p-10">
+          <h2 className="text-2xl text-white font-bold">{title}</h2>
+          <p className="text-white">{children}</p>
+          {isVisited &&  <span className="text-green-600"> Visitata</span>}
+          {!isVisited &&  <span className="text-red-600"> Non Visitata</span>}
         </div>
       </div>
-    </div>
+  
   );
 }
 export default Card;
