bcb83ead9af212310562a23894e5f62ac2e0b7be
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-04-27 16:00:29 +0200

    stile generalwe card

--- corso-react-Midali/src/App.tsx
+++ corso-react-Midali/src/App.tsx
@@ -15,7 +15,10 @@ function App() {
   const [data, setData] = useState<Post[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const [formState, dispatchFormState] = useReducer(formReducer, {name:"", email:""})
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    name: "",
+    email: "",
+  });
 
   useEffect(() => {
     async function fetchPosts() {
@@ -130,9 +133,8 @@ function App() {
     ));
   }
 
-
-  function formReducer (state:FormState, action:FormAction):FormState {
-    switch(action.type){
+  function formReducer(state: FormState, action: FormAction): FormState {
+    switch (action.type) {
       case "CHANGE_FIELD":
         if (action.field && action.field in state) {
           return { ...state, [action.field]: action.value };
@@ -140,27 +142,27 @@ function App() {
           return state;
         }
       case "RESET_FORM":
-        return{name:"", email:""}
+        return { name: "", email: "" };
       default:
         return state;
     }
   }
 
-  const handleFieldChange = (field:string, value:string) => {
-    dispatchFormState({type: "CHANGE_FIELD", field, value})
-  }
+  const handleFieldChange = (field: string, value: string) => {
+    dispatchFormState({ type: "CHANGE_FIELD", field, value });
+  };
 
   const resetForm = (e?: React.MouseEvent<HTMLButtonElement>) => {
     if (e) {
       e.preventDefault();
     }
-    dispatchFormState({type: "RESET_FORM"})
-  }
+    dispatchFormState({ type: "RESET_FORM" });
+  };
 
-  const sendForm = (e: React.MouseEvent<HTMLButtonElement>)=>{
-    e.preventDefault()
+  const sendForm = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
     console.log(formState);
-  }
+  };
 
   return (
     <>
@@ -195,7 +197,37 @@ function App() {
         </div>
         <CardForm addCity={addCity}></CardForm>
 
-        <div className="grid grid-cols-4 justify-items-center">
+        <form className="bg-slate-200 p-10 mt-3">
+        <div className="text-center pt-10 text-white">
+          <div  className="flex flex-col">
+            <label className="text-black" htmlFor="name">Nome:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="bg-slate-800 mb-2"
+              value={formState.name}
+              onChange={(e) => handleFieldChange("name", e.target.value)}
+            />
+          </div>
+
+          <div  className="flex flex-col">
+            <label className="text-black" htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="bg-slate-800 mb-2"
+              value={formState.email}
+              onChange={(e) => handleFieldChange("email", e.target.value)}
+            />
+          </div>
+          <button className="bg-teal-400 p-2 m-3  text-white font-bold" onClick={resetForm}>Reset</button>
+          <button className="bg-teal-400 p-2 m-3 text-white font-bold" onClick={sendForm}>Invia</button>
+        </div>
+      </form>
+
+        <div className="grid grid-cols-2 justify-items-center">
           {showVisited && visited()}
           {showNotVisited && NotVisited()}
           {showCity && allCity()}
@@ -206,29 +238,7 @@ function App() {
         {showPost && renderPosts()}
       </div>
 
-      <form>
-        <div>
-          <label htmlFor="name">Nome:</label>
-          <input  
-            type="text"
-            id="name"
-            name="name"
-            value={formState.name}
-            onChange={(e)=>handleFieldChange("name", e.target.value)}/>
-        </div>
-
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input  
-            type="email"
-            id="email"
-            name="email"
-            value={formState.email}
-            onChange={(e)=>handleFieldChange("email", e.target.value)}/>
-        </div>
-        <button onClick={resetForm}>Reset</button>
-        <button onClick={sendForm}>Invia</button>
-      </form>
+     
     </>
   );
 }
--- corso-react-Midali/src/components/Card.tsx
+++ corso-react-Midali/src/components/Card.tsx
@@ -6,7 +6,7 @@ import { City } from "../interfaces/City";
 function Card({ title, img, isVisited, description}:City):JSX.Element{
 
   return (
-    <div className="boxCard bg-current text-center my-5">
+    <div className="boxCard  bg-current text-center my-5">
       <img className=" imgCard  m-auto" src={img} alt="" />
         <div className="p-10">
           <h2 className="text-2xl text-white font-bold">{title}</h2>
